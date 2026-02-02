b168f90a43e2b2840c153dc424b607dc75d62f44
Justin Le Louedec <justinlelouedec@Justins.local>
2026-02-02 14:27:35 +0100

    updated sdo video creation for using quicklook fits files rather than previous quicklook processed pngs not available anymore 2

--- fetch_SDO.py
+++ fetch_SDO.py
@@ -55,10 +55,11 @@ def get_last_x_days_SDO(duration=7,path_to_save="/perm/aswo/ops/corona/"):
         global global_start 
         global_start = cnt
 
-        # pool= mp.get_context('fork').Pool(processes=12)
-        # pool.map(multi_processes_dl, np.arange(0,len(urls),1))
-        # pool.close()
-        # pool.join()
+        pool= mp.get_context('fork').Pool(processes=12)
+        pool.map(multi_processes_dl, np.arange(0,len(urls),1))
+        pool.close()
+        pool.join()
+        
         sdoaia193 = matplotlib.colormaps['sdoaia193']
         files = natsorted(glob.glob(temp_path+"/*"))
         for f in files:
